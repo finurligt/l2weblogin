1cd267b8a5b898f54a0bb0a9998b510bf789cc63
Fabian Sundholm <fabian.sundholm@hotmail.com>
2019-11-01 16:27:12 +0100

    finnish login feature

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    window.alert(user + " just signed in.")
+  } else {
+    // No user is signed in.
+  }
 });
+
+
+
+function login() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    window.alert(errorMessage);
+  });
+}
